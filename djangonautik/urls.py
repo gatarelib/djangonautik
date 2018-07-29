b54e65c812d03257189be114fd3be02a59e6bb60
root <gogia.prateek@hotmail.com>
2018-07-29 22:03:21 +0530

    Served different view pages for diffrent urls

--- djangonautik/urls.py
+++ djangonautik/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^about/$',),
-    path(r'^$', )
+    re_path(r'^about/$', views.about),
+    re_path(r'^$', views.homepage),
 ]
