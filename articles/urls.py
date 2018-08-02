167ebc93429bbcb700f7c7be66bc9585935324b1
root <gogia.prateek@hotmail.com>
2018-08-02 11:21:51 +0530

    Created an APIView

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-    url(r'^$', views.article_list),
-    url(r'^(?P<slug>[\w-]+)/$', views.article_detail),   
+    url(r'^$', views.article_list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),   
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles':articles})
 def article_detail(request, slug):
     return HttpResponse(slug)
+
+class HellloApiView(APIView):
+    """Test API View """
+
+    def get(self,request,format=None):
+        """Returns a list of APIView features. """
+
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)'
+            'It is similar to a traditional Django view',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs'
+
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
