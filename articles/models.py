0557e8b382d7bbccc97aacf6436cc22008976798
root <gogia.prateek@hotmail.com>
2018-08-16 09:31:52 +0530

    Added media for the particular post

--- articles/models.py
+++ articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    # add in thumbnail later
+    thumb = models.ImageField(default="default.png", blank=True)
     # add in author later
 
 #Indentation should be according to class, therefore it should be inside the class function otherwise it won't work
